54904eae4573ace9331480e332d55c2b20d84996
Edgard Thobias <edgardsoares.ed@gmail.com>
2020-05-19 11:49:24 -0400

    First Commit
    Primeiro commit de test
    
    Adicionando o .gitignore

---
+++ Hearth.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Hearth : MonoBehaviour
+{
+    public int _health;
+    public int _numOfHearts;
+
+    public Image[] _hearts;
+    public Sprite _fullHeart;
+    public Sprite _emptyHeart;
+
+
+    void Update()
+    {
+       
+        for (int i = 0; i < _hearts.Length; i++)
+        {
+            if(i < _health)
+            {
+                _hearts[i].sprite = _fullHeart;
+            }
+            else
+            {
+                _hearts[i].sprite = _emptyHeart;
+            }
+            
+            if(i < _numOfHearts)
+            {
+                _hearts[i].enabled = true;
+            }
+            else
+            {
+                _hearts[i].enabled = false;
+            }
+        }
+    }
+}
